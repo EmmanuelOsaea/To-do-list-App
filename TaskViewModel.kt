770325129918cf0b64c6dc9bfb91dbc18285cebc
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-28 06:44:38 +0100

    Create TaskViewModel.kt

---
+++ TaskViewModel.kt
@@ -0,0 +1,23 @@
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: TaskRepository
+    val allTasks: LiveData<List<Task>>
+
+    init {
+        val taskDao = TaskDatabase.getDatabase(application).taskDao()
+        repository = TaskRepository(taskDao)
+        allTasks = repository.allTasks
+    }
+
+    fun insert(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(task)
+    }
+
+    fun update(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(task)
+    }
+
+    fun delete(task: Task) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(task)
+    }
+}
