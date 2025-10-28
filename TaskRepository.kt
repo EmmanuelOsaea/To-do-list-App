7804a3a942c48f963f85392b97f81d6e533ab196
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-28 07:42:23 +0100

    Update TaskRepository.kt

--- TaskRepository.kt
+++ TaskRepository.kt
@@ -1,3 +1,9 @@
+package com.example.todoapp.data.repository
+import com.example.todoapp.data.local.TaskDao
+import com.example.todoapp.data.local.TaskEntity
+
+
+
 class TaskRepository(private val taskDao: TaskDao) {
 
     val allTasks: LiveData<List<Task>> = taskDao.getAllTasks()
