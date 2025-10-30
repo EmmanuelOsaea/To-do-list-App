9395f0faa9bc39d4c80a3a490ffba82537ac0f8d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-10-30 02:48:30 +0100

    Update Task.kt

--- Task.kt
+++ Task.kt
@@ -1,8 +1,13 @@
+package com.example.todoapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 @Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val description: String,
     val isCompleted: Boolean = false,
-    val dateCreated: Long = System.currentTimeMillis()
+    val dueTime: Long? = null
 )
