523f6df2b04bf910e05d6a19c7e489a8a892a21b
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-09 13:22:07 +0100

    Update TaskDatabase.kt

--- TaskDatabase.kt
+++ TaskDatabase.kt
@@ -1,13 +1,13 @@
 package com.example.todoapp.data.local
-package com.example.todoapp.data
 
 import android.content.Context
 import androidx.room.Database
-import androidx.room.RoomDatabase
 import androidx.room.Room
+import androidx.room.RoomDatabase
 
-@Database(entities = [Task::class], version = 1, exportSchema = false)
+@Database(entities = [TaskEntity::class], version = 1, exportSchema = false)
 abstract class TaskDatabase : RoomDatabase() {
+
     abstract fun taskDao(): TaskDao
 
     companion object {
